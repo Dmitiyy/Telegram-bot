d64592933d61d4a014bf9dc9d6fa18c6dfaeb818
Dmitriy <dmitriyy311@gmail.com>
2021-03-24 09:15:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+    const token = '1602790385:AAHphMQW8Oa-QaLLwfm_0qxWfUJ-vf9Cvfk';
+    $link = 'https://api.telegram.org/bot'.token;
+
+    $updates = file_get_contents('php://input');
+    $updates = json_decode($updates, true);
+
+    $msg_id = $updates['message']['from']['id'];
+    $text = $updates['message']['text'];
+
+    function sendMsg () {
+        global $link;
+        global $msg_id;
+        global $text;
+
+        $url = $link.'/sendMessage?chat_id='.$msg_id.'&text='.urlencode($text);
+        file_get_contents($url);
+    }
+?>
